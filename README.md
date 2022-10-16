eea619f31ff48b1aafba13e2579c90b580f17222
Dany Marcoux <dmarcoux@posteo.de>
2022-10-16 23:00:07 +0200

    Create development environment with nix-shell

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # <a href="https://github.com/dmarcoux/elixir_templates">dmarcoux/elixir_templates</a>
 
 Templates for common files/configs I use in Elixir projects.
+
+## nix-shell
+
+Reproducible development environment for Elixir and
+[Phoenix](https://www.phoenixframework.org) projects which relies on
+[Nix](https://github.com/NixOS/nix), a purely functional and cross-platform
+package manager.
+
+### Usage
+
+1. Copy at the root of the Elixir/Phoenix project.
+2. Start with `nix-shell --pure`.
---
+++ shell.nix
@@ -0,0 +1,35 @@
+# To ensure this nix-shell is reproducible, we pin the packages index to a commit SHA taken from a channel on https://status.nixos.org/
+# This commit is from NixOS 22.05
+with (import (fetchTarball https://github.com/NixOS/nixpkgs/archive/8de8b98839d1f20089582cfe1a81207258fcc1f1.tar.gz) {});
+
+mkShell {
+  buildInputs = [
+    # Elixir 1.14 with Erlang/OTP 25 (relying on the package `elixir_1_14` alone isn't enough, as the version of Erlang cannot be specified)
+    beam.packages.erlangR25.elixir_1_14
+    # For the Live Reloading feature in Phoenix
+    inotify-tools
+    # Database PostgreSQL
+    postgresql_14
+    # Locales
+    glibcLocales
+  ];
+
+  shellHook = ''
+    # Set LANG for locales, otherwise it is unset when running "nix-shell --pure"
+    export LANG="C.UTF-8"
+
+    # Set development environment for Mix
+    export MIX_ENV=dev
+
+    # Install Hex
+    mix local.hex --force
+
+    # Install the Phoenix application generator
+    mix archive.install --force hex phx_new
+  '';
+
+  # Without this, there are warnings about LANG, LC_ALL and locales.
+  # Many tests fail due those warnings showing up in test outputs too...
+  # This solution is from: https://gist.github.com/aabs/fba5cd1a8038fb84a46909250d34a5c1
+  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+}
