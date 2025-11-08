72c71932128d2148d83274a37547682bc7322acb
Dany Marcoux <git@dmarcoux.com>
2025-11-08 11:53:52 +0100

    Fix File.Error in .iex.exs
    https://hexdocs.pm/iex/1.19.2/IEx.Helpers.html#import_file_if_available/1

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,5 @@
 # Import the global configuration for IEx if it's present, so anybody can keep
 # their custom IEx configuration on top of project-specific configurations.
-File.exists?(Path.expand("~/.iex.exs")) && import_file("~/.iex.exs")
+import_file_if_available("~/.iex.exs")
 
 # Add project-specific configuration here...
